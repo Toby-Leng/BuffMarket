120aa38644c62eea7c58ba88761980f60b5c5a98
TobyCold <tobycold@qq.com>
2023-09-22 23:15:56 +0800

    TobyCold

---
+++ src/main/java/love/iloveworld/buffmoitor/BuffMoitorApplication.java
@@ -0,0 +1,14 @@
+package love.iloveworld.buffmoitor;
+
+import love.iloveworld.buffmoitor.test.Index;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuffMoitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuffMoitorApplication.class, args);
+    }
+
+}
